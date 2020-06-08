26ed611866c758975c4f32f51a68d8ce224b9aee
carguti <carlosgutierrezcasado.1991@gmail.com>
2020-06-08 19:00:39 +0200

    Initial commit

---
+++ RSSReader/RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Carlos Gutierrez Casado on 08/06/2020.
+//  Copyright © 2020 Carlos Gutierrez Casado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
