0f553b1b1aa4dc4166c0b4ecd19332cf3912ff73
carguti <carlosgutierrezcasado.1991@gmail.com>
2020-06-08 19:53:50 +0200

    Added initial structure and configuration

---
+++ RSSReader/RSSReader/Coordinator/CoordinatorProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  CoordinatorProtocol.swift
+//  RSSReader
+//
+//  Created by Carlos Gutierrez Casado on 08/06/2020.
+//  Copyright © 2020 Carlos Gutierrez Casado. All rights reserved.
+//
+
+import Foundation
---
+++ RSSReader/RSSReader/Coordinator/GeneralCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  GeneralCoordinator.swift
+//  RSSReader
+//
+//  Created by Carlos Gutierrez Casado on 08/06/2020.
+//  Copyright © 2020 Carlos Gutierrez Casado. All rights reserved.
+//
+
+import Foundation
